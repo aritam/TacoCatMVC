d5677a99355e5b1fefaa046b494d988ff9ea1af8
aritam <50036325+aritam@users.noreply.github.com>
2022-06-22 17:12:41 -0500

    Update on project
    Added index page, and final functionality.

--- TacoCatMVC/Controllers/HomeController.cs
+++ TacoCatMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using TacoCatMVC.Models;
 
@@ -22,6 +23,45 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    [HttpGet]
+    public IActionResult Reverse()
+    {
+        Palindrome model = new();
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Reverse(Palindrome palindrome)
+    {
+        string inputWord = palindrome.InputWord;
+        string revWord = "";
+
+        for (int i = inputWord.Length - 1; i >= 0; i--)
+        {
+            revWord += inputWord[i];
+        }
+
+        palindrome.RevWord = revWord;
+        
+        revWord = Regex.Replace(revWord.ToLower(),"[^a-zA-Z0-9]+","");
+        inputWord = Regex.Replace(inputWord.ToLower(),"[^a-zA-Z0-9]+","");
+
+        if (revWord == inputWord)
+        {
+            palindrome.IsPalindrome = true;
+            palindrome.Message = $"Success! '{palindrome.InputWord}' is a palindrome :3 ";
+        }
+        else
+        {
+            palindrome.IsPalindrome = false;
+            palindrome.Message = $"Sorry {palindrome.InputWord} is not a palindrome :o ";
+        }
+
+        return View(palindrome);
+    }
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
